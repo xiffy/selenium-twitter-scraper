34ebb9e91a78f1f93f07240af806d3a0e7a3f9ea
xiffy <xiffy@xiffy.nl>
2024-03-18 21:21:02 +0100

    minder babbels

--- scraper/progress.py
+++ scraper/progress.py
@@ -10,16 +10,17 @@ class Progress:
     def print_progress(self, current) -> None:
         self.current = current
         progress = current / self.total
-        bar_length = 40
-        progress_bar = (
-            "["
-            + "=" * int(bar_length * progress)
-            + "-" * (bar_length - int(bar_length * progress))
-            + "]"
-        )
-        sys.stdout.write(
-            "\rProgress: [{:<40}] {:.2%} {} of {}".format(
-                progress_bar, progress, current, self.total
-            )
-        )
-        sys.stdout.flush()
+        print(progress, end=" ")
+        # bar_length = 40
+        # progress_bar = (
+        #     "["
+        #     + "=" * int(bar_length * progress)
+        #     + "-" * (bar_length - int(bar_length * progress))
+        #     + "]"
+        # )
+        # sys.stdout.write(
+        #     "\rProgress: [{:<40}] {:.2%} {} of {}".format(
+        #         progress_bar, progress, current, self.total
+        #     )
+        # )
+        # sys.stdout.flush()
